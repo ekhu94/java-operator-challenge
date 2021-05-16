78b3b77c08ca63bab0a06702f94d48cf85d38f27
Erik Huang <ekhu94@gmail.com>
2021-05-16 08:04:28 -0700

    Completes own implementation of challenge

--- src/com/ekhu94/Main.java
+++ src/com/ekhu94/Main.java
@@ -3,6 +3,14 @@ package com.ekhu94;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    double myDouble = 20.00;
+	    double mySecondDouble = 80.00;
+	    double doubleSum = myDouble + mySecondDouble;
+	    double doubleRemainder = doubleSum % 40.00;
+	    boolean isZero = doubleRemainder == 0;
+        System.out.println(isZero);
+        if (!isZero) {
+			System.out.println("Got some remainder");
+		}
     }
 }
