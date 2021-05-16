b503633df44ed917c11ecd64efdae49ada1c6ffa
Erik Huang <ekhu94@gmail.com>
2021-05-16 08:09:17 -0700

    done

--- src/com/ekhu94/Main.java
+++ src/com/ekhu94/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    double myDouble = 20.00;
 	    double mySecondDouble = 80.00;
-	    double doubleSum = myDouble + mySecondDouble;
+	    double doubleSum = (myDouble + mySecondDouble) * 100.00;
 	    double doubleRemainder = doubleSum % 40.00;
 	    boolean isZero = doubleRemainder == 0;
         System.out.println(isZero);
